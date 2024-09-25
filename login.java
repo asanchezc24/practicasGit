abfef1e49bb3864256dea4e762623d3bcb22663d
Fabrizzio <asanchezc@uni.pe>
2024-09-25 09:03:15 -0500

    actualización del login

--- login.java
+++ login.java
@@ -2,6 +2,7 @@ public class login {
     public static void main(String[] args) {
         System.out.println("Creando el login");
         System.out.println("trabajando en la segunda versión del login ");
+        System.out.println("...");
     }
     
 }

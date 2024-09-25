a1a3c6ebe7c1b074381673e5c001da39eb06eca6
Fabrizzio <asanchezc@uni.pe>
2024-09-25 08:53:49 -0500

    segunda versión del login

--- login.java
+++ login.java
@@ -1,6 +1,7 @@
 public class login {
     public static void main(String[] args) {
         System.out.println("Creando el login");
+        System.out.println("trabajando en la segunda versión del login ");
     }
     
 }

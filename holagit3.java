0824394b21350c130d36d6bf45477891b1fb1c93
Fabrizzio <asanchezc@uni.pe>
2024-09-25 08:27:15 -0500

    modificación de git3 en login

--- holagit3.java
+++ holagit3.java
@@ -2,6 +2,7 @@ public class holagit3 {
     public static void main(String[] args) {
         System.out.println("Mi tercer saludo");
         System.out.println("Versión 2 del saludo 3, corregido");
+        System.out.println("Modificación del holagit3 en rama login");
     }
     
 }

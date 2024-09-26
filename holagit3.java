59a9283baabe2b4847713551e4cfb28b955c3c4d
Fabrizzio <asanchezc@uni.pe>
2024-09-26 11:35:37 -0500

    modificacion del saludo, agregué un hola reparado

--- holagit3.java
+++ holagit3.java
@@ -3,6 +3,7 @@ public class holagit3 {
         System.out.println("Mi tercer saludo");
         System.out.println("Versión 2 del saludo 3, corregido");
         System.out.println("Segumos mejorando el hellogit3");
+        System.out.println("hola reparado");
     }
     
 }

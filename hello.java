44e571876424da8d7a1b7ec378654d13351fe057
Fabrizzio <asanchezc@uni.pe>
2024-09-26 14:22:19 -0500

    aqui se mejora el hello, desde main

--- hello.java
+++ hello.java
@@ -4,6 +4,8 @@ public static void main(String[] args) {
     System.out.println("Otro hello");
     System.out.println("Implementación de hello en el main local");
     System.out.println("nueva versión del hello");
+    //mejoras al hello
+    System.out.println("aqui se mejora el hello, desde main");
 }
 
 }
